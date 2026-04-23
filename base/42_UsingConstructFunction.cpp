d8889158e12d5cdcf22acb2082e408646c5a6710
stubbornmonkey <2696283321@qq.com>
2026-04-23 21:24:17 +0800

    2026-4-23

---
+++ base/42_UsingConstructFunction.cpp
@@ -0,0 +1,46 @@
+//
+// Created by 倔强的猴子 on 2026/4/23.
+//
+/*
+#include <iostream>
+using namespace std;
+
+class Person{
+    // define construct function
+public:
+    // construct function Overload
+    Person() {
+        cout << "This is Person()" << endl;
+    }
+
+    // ban to implicit use
+    explicit Person(int age) {
+        cout << "This is Person(int)" << endl;
+    }
+
+    Person(int age,int score) {
+        cout << "This is Person(int,int)" << endl;
+    }
+};
+
+int main() {
+
+    // explicit use
+    //Person person;
+    //Person person = Person(10);
+    //Person person(10);
+    //Person person = Person(10,20);
+    //Person person(10,10);
+
+    // implicit use
+    //Person person = {};
+    //Person person = {10};
+    //Person person = {10,10};
+
+
+    // on heap
+    //Person* person = new Person();
+
+    return 0;
+}
+ */
---
+++ base/43_ConstructionPercautions.cpp
@@ -0,0 +1,25 @@
+//
+// Created by 倔强的猴子 on 2026/4/23.
+//
+/*
+#include <iostream>
+using namespace std;
+
+// attention: If class is no Construction,system provide public without participant construction to class
+// If class have participant construction,without participant construction is destroy,if you want to use, you can define one
+class Person{
+public:
+    Person(){
+
+    }
+
+    Person(int age) {
+
+    }
+};
+int main() {
+    Person person;
+
+    return 0;
+}
+ */
