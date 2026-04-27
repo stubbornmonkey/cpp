4798e875bd5d37fdd89f29415a08d71594629b06
stubbornmonkey <2696283321@qq.com>
2026-04-27 20:23:06 +0800

    2026-4-27

--- base/55_OperatorOverload.cpp
+++ base/55_OperatorOverload.cpp
@@ -1,6 +1,7 @@
 //
 // Created by 倔强的猴子 on 2026/4/25.
 //
+/*
 #include <iostream>
 using namespace std;
 
@@ -17,6 +18,19 @@ public:
     Point operator-(const Point& p) {
         return {x - p.x,y - p.y};
     }
+
+    Point operator--() {
+        x++;
+        y++;
+        return *this;
+    }
+
+    Point operator--(int) {
+        Point tmp = *this;
+        x++;
+        y++;
+        return tmp;
+    }
 };
 
 // use global function implement operator overload
@@ -25,6 +39,22 @@ Point operator+(const Point& p1,const Point& p2) {
     // implicit create object
     return {p1.x + p2.x,p1.y + p2.y};
 }
+
+// before value
+Point operator++(Point& p) {
+    p.x++;
+    p.y++;
+    return p;
+}
+
+// after value
+Point operator++(Point& p,int) {
+    Point tmp = p;
+    p.x++;
+    p.y++;
+    return tmp;
+}
+
 
 int main() {
 
@@ -39,5 +69,14 @@ int main() {
 
     cout << "p4.x = " << p4.x << ",p4.y = " << p4.y << endl;
 
+    Point p5 = ++p1;
+    cout << "x = " << p5.x << endl;
+    cout << "y = " << p5.y << endl;
+
+    Point p6 = p1++;
+    cout << "x = " << p6.x << endl;
+    cout << "y = " << p6.y << endl;
+
     return 0;
 }
+ */
---
+++ base/56_OverloadOperatorLeftMove.cpp
@@ -0,0 +1,29 @@
+//
+// Created by 倔强的猴子 on 2026/4/27.
+//
+#include <iostream>
+using namespace std;
+
+class Person{
+    friend ostream& operator<<(ostream& os,const Person& p);
+private:
+    string name;
+    int age;
+    string gender;
+    int* score;
+public:
+    Person() : name(""),age(0),gender("male"),score(new int(0)){};
+    Person(string name,int age,string gender,int score):name(name),age(age),gender(gender),score(new int(score)){};
+};
+
+// overload <<
+ostream& operator<<(ostream& os,const Person& p) {
+    os << "name = " << p.name << ",age = " << p.age << ",gender = " << p.gender << ",score = " << *p.score << endl;
+    return os;
+}
+
+int main() {
+    Person p("p",10,"male",100);
+    cout << p << endl;
+    return 0;
+}
